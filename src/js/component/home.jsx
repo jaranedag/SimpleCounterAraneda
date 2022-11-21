5e6d70a3e94d4a66f23e9704e416a45113e3fafe
jaranedag <jaranedag@udd.cl>
2022-11-21 14:35:10 +0000

    tarea contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,33 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = ({segundos}) => {
+	let string = segundos.toString()
+	let digitOne = string[string.length -1]
+	let digitTwo = string[string.length -2]
+	let digitThree = string[string.length -3]
+	let digitFour = string[string.length -4]
+	let digitFive = string[string.length -5]
+	let digitSix = string[string.length -6]
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+             <div className="text-center">
+				<button className="btn btn-dark"><i class="far fa-clock"></i></button>
+				<button className="btn btn-dark">{digitSix==undefined? "0": digitSix}</button>
+                <button className="btn btn-dark">{digitFive==undefined? "0": digitFive}</button>
+				<button className="btn btn-dark">{digitFour ==undefined? "0": digitFour}</button>
+				<button className="btn btn-dark">{digitThree==undefined? "0": digitThree}</button>
+				<button className="btn btn-dark">{digitTwo==undefined? "0": digitTwo}</button>
+				<button className="btn btn-dark">{digitOne==undefined? "0": digitOne}</button>
+			 </div>
 	);
 };
 
 export default Home;
+
+/*
+<div className="container" >
+
+<div className="row  bg-dark" style = {{height: 150}} > */
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,12 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let tiempo =  0;
+
+setInterval(()=>{
+    console.log(tiempo);
+    ReactDOM.render( <Home segundos={tiempo} />, document.querySelector("#app"));
+
+    tiempo++
+},1000)
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
